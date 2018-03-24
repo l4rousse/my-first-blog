c1b8a382fb0c43631e2ed9b0c28b3645377a52df
l4rousse <www.ameunier@gmail.com>
2018-03-24 10:48:20 +0100

    My Django Girls app, first commit

---
+++ exercice/Hello.py
@@ -0,0 +1,23 @@
+def hi(name):
+	print("Hi " + name + "!")
+hi("Rachel")
+print('Hello, Django girls!')
+if 1 > 2:
+	print("it works!")
+if 5>2:
+	print("5 is indeed greater than 2")
+else:
+	print ("5 is not greater than 2")
+name = "Sonja"
+if name == "Ola":
+	print("Hey Ola!")
+elif name == "Sonja":
+	print("Hey Sonja")
+else:
+	print("Hey anonymous!")
+girls = ['Rachel', 'Monica', 'Phoebe', 'Ola', 'You']
+for name in girls:
+	hi(name)
+	print("Next girl")
+for i in range (1, 6):
+	print(i)
